efa095b6cf6f770ee40719d6c345dc32a461326b
adrien-jiang <3134115374@qq.com>
2025-11-09 17:07:26 +0800

    Add files via upload

---
+++ 黑马程序.py
@@ -0,0 +1,129 @@
+#公司情况格式化
+# company_name = "传智播客"
+# size_num = "00302"
+# prize_num = "19.99"
+# print(f"本公司名为：{company_name},股票代码是：{size_num},当前股价是：{prize_num}")
+#股价升高格式化
+# zz_num = 1.2
+# ti_num = 7
+# now_num = 71.63
+# print("增长系数是：%.2f,经历时间是：%d,股价达到了：%.2f"%(zz_num,ti_num,now_num))
+# a =10
+# guess1=int(input("number:"))
+# if guess1==a:
+#     print("nb")
+# else:
+#     guess2=int(input("number:"))
+#     if guess2 ==a:
+#         print("nb")
+#     else:
+#         guess3=int(input("number:"))
+#         if guess3==a:
+#             print("nb")
+#         else:
+#             print("douchuo")
+# 定义心里想的数字（这里设为 10，可随意修改）
+
+
+# 第一次猜
+# guess1 = int(input("请输入第一次猜想的数字："))
+# if guess1 == a:
+#     print("猜对啦！")
+# else:
+#     # 第二次猜
+#     guess2 = int(input("不对，再猜一次："))
+#     if guess2 == a:
+#         print("猜对啦！")
+#     else:
+#         # 第三次猜
+#         guess3 = int(input("不对，再猜最后一次："))
+#         if guess3 == a:
+#             print("猜对啦！")
+#         else:
+#             print(f"Sorry，全部猜错啦，我想的是：{a}")
+#“”猜数字
+# a =10
+# guess1=int(input("number:"))
+# if guess1==a:
+#     print("nb")
+# else:
+#     guess2=int(input("number:"))
+#     if guess2 ==a:
+#         print("nb")
+#     else:
+#         guess3=int(input("number:"))
+#         if guess3==a:
+#             print("nb")
+#         else:
+#             print("douchuo")
+# x = float(input("x="))
+# result=(1/x)
+# if x ==10 :
+#     print(f"f({x})={result}")
+# else:
+#     print(f"f({x})={x}")
+# x = float(input())
+# result=(1/x)
+# if x ==10 :
+#     print(f"f({x}) = {result}")
+# else:
+#     print(f"f({x}) = {x}")
+# num = 5
+# if int(input("猜想数字:")) == num:
+#     print("niubi")
+# elif int(input("戳啦，再来"))== num:
+#     print("niubi")
+# elif int(input("又戳啦，再来")) == num:
+#     print("niubi")
+# else :
+#     print("戳玩啦")
+# if int(input("身高："))>120:
+#     print("no free")
+#     print("but have a chance")
+#     if int(input("how many your level"))>3:
+#         print("free")
+#     else :
+#         print("10 doller")
+# else:
+#     print("welcome child")
+
+
+# import random
+# num = random.randint(1,10)
+# guess_num = int(input("猜测数字："))
+# if num == guess_num:
+#     print("right!")
+# else:
+#     if guess_num > num :
+#         print("大了")
+#     elif guess_num < num :
+#         print("小了")
+# guess_num = int(input("猜测数字："))
+# if num == guess_num:
+#     print("right!")
+# else:
+#     if guess_num > num :
+#         print("大了")
+#     elif guess_num < num :
+#         print("小了")
+# guess_num = int(input("猜测数字："))
+# if num == guess_num:
+#     print("right!")
+# else:
+#     if guess_num > num :
+#         print("大了")
+#     elif guess_num < num :
+#         print("小了")
+
+
+#while循环
+# i = 0
+# while i < 100:
+#     print("马雪霞")
+#     i+=1
+# i=1
+# s=0
+# while i<=100:
+#     s+=i
+#     i+=1
+#     print(s)
